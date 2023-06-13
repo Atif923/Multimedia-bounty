693889ddfa6caf7687ba1cc90028fd463cca9940
Atif923 <109849603+Atif923@users.noreply.github.com>
2023-06-13 15:39:44 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/SampleVideo.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/SampleAudio.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/pdf_1.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/SampleImage_1.png"
+    },
+    {
+        id: 5,
+        name: 'Image File 2',
+        type: 'image',
+        path: "/file-server/SampleImage_2.png"
+    },
+    {
+        id: 6,
+        name: 'Image File 3',
+        type: 'image',
+        path: "/file-server/SampleImage_3.png"
+    },
+  ]
